8b2f4262de2101ce52e0bed402b555721f4ed864
ABDULLAH QIDWAI <abdullahqidwai92@gmail.com>
2026-04-28 17:10:56 +0530

    Update API endpoint for password

--- client/src/components/login-popup/login.jsx
+++ client/src/components/login-popup/login.jsx
@@ -84,7 +84,7 @@ try {
         toast.error("ENTER  PASSWORD AND EMAIL");
         return ;
     }
-        const response=await axios.post("http://localhost:8000/api/auth/new",{newpassword,email:logindatastructure.email});
+        const response=await axios.post("https://qx-editor-backend.onrender.com/api/auth/new",{newpassword,email:logindatastructure.email});
         if(response.data.status){
             toast.success(response.data.message);
         }
