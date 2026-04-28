978eef645fe2daa3699b9b46ff3c522ef85f4d44
ABDULLAH QIDWAI <abdullahqidwai92@gmail.com>
2026-04-28 17:37:02 +0530

    Change SameSite cookie attribute from 'strict' to 'none'

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -55,7 +55,7 @@ const login=async(req,res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             secure:true,
-            sameSite:"strict",
+            sameSite:"none",
             maxAge:24*60*60*1000
         })
 
